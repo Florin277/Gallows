2d88965afe3409e3ac76d5c42db2bfe8d225cc99
Florin277 <simoncaflorin79@gmail.com>
2025-05-15 08:56:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const words =['House','Welcome','Wellcode'];
+const wordNumber = getRandomInt(words.length);
+let chosenWord = words[wordNumber];
+let numberLetter = chosenWord.length;
+let livesNo = 7;
+let positionsOfChosenWord = '';
+document.getElementById('lineNo').innerHTML=chosenWord.length;
+document.getElementById('lives').innerHTML=livesNo;
+for (let i = 0; i < chosenWord.length; ++i) {
+  positionsOfChosenWord += "_ ";
+}
+document.getElementById('line').innerHTML=positionsOfChosenWord;
+chosenWord = chosenWord.split('');
+let arrayPositionsOfChosenWord = positionsOfChosenWord.split('');
+function searchLetters() {
+  let foundLetter = 0;
+  let letterLooking = document.getElementById('searchLetter').value;
+  for (let i = 0; i < chosenWord.length; ++i) {
+    let copychosenWord = chosenWord[i];
+    if (letterLooking[0].toLowerCase() === chosenWord[i].toLowerCase())  {
+      foundLetter = 1;
+      arrayPositionsOfChosenWord.splice(i * 2, 1, copychosenWord);
+      --numberLetter;
+      chosenWord.splice(i, 1, '0');
+    } 
+    if (i == chosenWord.length - 1 && foundLetter == 0) {
+      --livesNo;
+      foundLetter = 0;
+      document.getElementById('lives').innerHTML=livesNo;  
+    }
+  }
+  if (livesNo == 0) {
+    document.getElementById('message').innerHTML = "Unfortunately you LOST 😒";
+  } else if (numberLetter == 0) {
+    document.getElementById('message').innerHTML = "You are a WINNER!🎉";
+  }
+  document.getElementById('line').innerHTML = arrayPositionsOfChosenWord.join('');
+  document.getElementById('searchLetter').value="";
+}
+
+function getRandomInt(totalWords) {
+    return Math.floor(Math.random() * totalWords);
+}
